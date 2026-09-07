5d0f5659b952c6988cc109888991930a090a64fe
polodevz_ <justwhite1488@gmail.com>
2026-09-07 19:01:58 +0300

    Update loader.lua

--- games/symbiote/loader.lua
+++ games/symbiote/loader.lua
@@ -1,1 +1,6 @@
+local a = game.PlaceId
 
+local b, c = pcall(game.HttpGet, 'https://raw.githubusercontent.com/justwhite1488-boop/skid.cc/refs/heads/main/games/symbiote/games/' .. a .. '.lua')
+if b then
+	loadstring(c)()
+end

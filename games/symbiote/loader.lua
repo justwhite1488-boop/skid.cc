f0202e03eb02a56eebc11a0caacd6ab661e2ea96
polodevz_ <justwhite1488@gmail.com>
2026-09-07 19:23:57 +0300

    Update loader.lua

--- games/symbiote/loader.lua
+++ games/symbiote/loader.lua
@@ -1,6 +1,11 @@
-local a = game.PlaceId
-
-local b, c = pcall(game.HttpGet, 'https://raw.githubusercontent.com/justwhite1488-boop/skid.cc/refs/heads/main/games/symbiote/games/' .. a .. '.lua')
-if b then
-	loadstring(c)()
+local a, b = pcall(game.HttpGet, 'https://raw.githubusercontent.com/justwhite1488-boop/skid.cc/refs/heads/main/games/symbiote/games/' .. game.PlaceId .. '.lua')
+if a then
+	loadstring(b)()
+else
+	local c, d = pcall(game.HttpGet, 'https://raw.githubusercontent.com/justwhite1488-boop/skid.cc/refs/heads/main/games/symbiote/games/' .. game.GameId .. '.lua')
+	if c then
+		loadstring(d)()
+	else
+		warn("game isn't supported.")
+	end
 end
